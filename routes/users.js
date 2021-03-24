a1057e076b80010877c102b4f59cb92f658bde54
manojnuvvala <manoj.nuvvala@gmail.com>
2021-03-24 01:26:51 -0500

    user.js file update

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
+var man = 0
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+  man = man + 1;
+
+  res.send('User accesses are:' + man);
 });
 
 module.exports = router;
